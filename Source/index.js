f3756143321e65e23d55e1335b1b518f72237fb2
kishore7860 <mamidikishorereddy@gmail.com>
2024-06-03 23:59:35 -0500

    Created route file

---
+++ Source/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import mongoose from "mongoose";
+import { PORT, MongoURL } from "./config.js";
+import contactRouter from "./routes/contactRoute.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+	cors({
+		"Access-Control-Allow-Origin": "*",
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		preflightContinue: false,
+		optionsSuccessStatus: 204,
+	})
+);
+
+// Middleware to log request details
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.path}`);
+	console.log("Body:", req.body);
+	next();
+});
+
+app.use("/contact", contactRouter);
+
+app.get("/", (req, res) => {
+	return res.status(200).send("<h1>Hello, World!</h1>");
+});
+
+mongoose
+	.connect(MongoURL)
+	.then(() => {
+		console.log("Connected to MongoDB");
+		app.listen(PORT, () => {
+			console.log(`Example app listening at ${PORT}`);
+			console.log(`Check http://localhost:${PORT}`);
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
