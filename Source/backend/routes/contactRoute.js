5f950a4e553f7fa12b1bbe7a58b19554e8de39fb
chiranjeevigundu <chiranjeevigund@gmail.com>
2024-06-05 00:54:06 -0500

    Notify email service updated

--- Source/backend/routes/contactRoute.js
+++ Source/backend/routes/contactRoute.js
@@ -1,36 +1,38 @@
 import express from "express";
 import { Contact } from "../models/Contact.js";
+import { sendEmail } from "../emailService.js";
 
 const contactRouter = express.Router();
 
 contactRouter.get("/", (req, res) => {
-	res.status(200).send("This is a contact");
+    res.status(200).send("This is a contact");
 });
 
 contactRouter.post("/createcontact", async (req, res) => {
-	try {
-		// console.log(req.body);
-		// const contact = new Contact(req.body);
-		// console.log(contact);
-		if (!req.body.name || !req.body.email || !req.body.phoneNumber) {
-			return res.status(400).send("Please provide required information!");
-		}
+    try {
+        if (!req.body.name || !req.body.email || !req.body.phoneNumber) {
+            return res.status(400).send("Please provide required information!");
+        }
 
-		const newContact = {
-			name: req.body.name,
-			email: req.body.email,
-			phoneNumber: req.body.phoneNumber,
-		};
-		if (req.body.description) {
-			newContact.description = req.body.description;
-		}
+        const newContact = {
+            name: req.body.name,
+            email: req.body.email,
+            phoneNumber: req.body.phoneNumber,
+            description: req.body.description,
+        };
 
-		const createdContact = await Contact.create(newContact);
-		return res.status(201).send(createdContact);
-	} catch (error) {
-		console.log(error.message);
-		return res.status(500).send({ message: error.message });
-	}
+        const createdContact = await Contact.create(newContact);
+
+        // Send an automated email
+        const subject = "Thank you for contacting us!";
+        const text = `Hello ${newContact.name},\n\nThank you for reaching out to us. We have received your query and will get back to you within 48 hours.\n\nBest regards,\nYour Company Name`;
+        sendEmail(newContact.email, subject, text);
+
+        return res.status(201).send(createdContact);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).send({ message: error.message });
+    }
 });
 
 export default contactRouter;
