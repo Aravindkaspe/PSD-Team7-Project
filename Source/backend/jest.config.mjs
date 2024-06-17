403770846fe75ab3bd960ddfcc8dc62a5ba9e249
Aravindkaspe <aravindkaspe1991@gmail.com>
2024-06-17 17:15:47 -0500

    Updated email test case file

---
+++ Source/backend/jest.config.mjs
@@ -0,0 +1,18 @@
+export default {
+    testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'json'],
+    rootDir: './',
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
+    coverageDirectory: './coverage',
+    collectCoverageFrom: ['**/*.{js,jsx}', '!**/node_modules/**', '!**/vendor/**'],
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest'
+    },
+    moduleNameMapper: {
+      '\\.css$': 'identity-obj-proxy',
+    },
+    moduleNameMapper: {
+      '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    },
+  };
+  
